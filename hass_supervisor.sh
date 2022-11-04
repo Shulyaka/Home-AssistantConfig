8dc38ad31ae85c90543c2fa88ffde1e1fabe1b53
Denis Shulyaka <Shulyaka@gmail.com>
2022-11-04 23:32:08 +0300

    Update hass_supervisor script for clean shutdown

--- hass_supervisor.sh
+++ hass_supervisor.sh
@@ -2,11 +2,22 @@
 
 PROG=$@
 
+trap 'kill -TERM "$CPID"' TERM
+trap 'kill -INT "$CPID"' INT
+
 RET=100
 while [ $RET == 100 ]; do
   echo Running "$PROG"
-  $PROG
-  RET=$?
+
+  $PROG &
+  CPID=$!
+
+  RET=129
+  while [ $RET -gt 128 ]; do
+    wait $CPID
+    RET=$?
+  done
+
   echo Return code $RET
 done
 
