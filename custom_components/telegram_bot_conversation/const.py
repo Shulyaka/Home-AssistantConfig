24494efd0aabbd7da575c5b727bab5e44cb0f95e
Denis Shulyaka <Shulyaka@gmail.com>
2026-03-10 16:34:38 +0300

    switch to release version of telegram_bot_conversation

--- custom_components/telegram_bot_conversation/const.py
+++
@@ -1,9 +0,0 @@
-"""Telegram Bot Conversation."""
-
-DOMAIN = "telegram_bot_conversation"
-
-CONF_CONVERSATION_TIMEOUT = "conversation_timeout"
-CONF_CONVERSATION_AGENT = "conversation_agent"
-CONF_TELEGRAM_ENTRY = "telegram_entry"
-CONF_TELEGRAM_SUBENTRY = "telegram_subentry"
-CONF_USER = "user_id"
